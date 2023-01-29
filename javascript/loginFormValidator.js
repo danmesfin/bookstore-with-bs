f54d4d82c12287c7158ee8eae0f2cef971789260
danmesfin <danielmsfn@gmail.com>
2023-01-29 23:11:04 +0300

    edit error-message

--- javascript/loginFormValidator.js
+++ javascript/loginFormValidator.js
@@ -1,25 +1,22 @@
 function validateForm() {
-    var nameVal = document.forms["simple__form"]["email"].value;
-    
-    if (nameVal == null || nameVal == "") {
-      document.getElementsByClassName(
-        "error--message"
-      )[0].style.visibility = "visible";
-      document.getElementsByClassName("error--message")[0].innerHTML =
-        "Please Fill out this field";
-      return false;
-    } 
+  var nameVal = document.forms["simple__form"]["email"].value;
 
-    var passwordVal = document.forms["simple__form"]["password"].value;
-    if (passwordVal == null || passwordVal == "") {
-      document.getElementsByClassName(
-        "error--message"
-      )[0].style.visibility = "visible";
-      document.getElementsByClassName("error--message")[0].innerHTML =
-        "Please Fill out this field";
-      return false;
-    } else {
-      return true;
-    }
-
+  if (nameVal == null || nameVal == "") {
+    document.getElementsByClassName("error--message")[0].style.visibility =
+      "visible";
+    document.getElementsByClassName("error--message")[0].innerHTML =
+      "Please Fill Username/Email";
+    return false;
   }
+
+  var passwordVal = document.forms["simple__form"]["password"].value;
+  if (passwordVal == null || passwordVal == "") {
+    document.getElementsByClassName("error--message")[0].style.visibility =
+      "visible";
+    document.getElementsByClassName("error--message")[0].innerHTML =
+      "Please enter password";
+    return false;
+  } else {
+    return true;
+  }
+}
